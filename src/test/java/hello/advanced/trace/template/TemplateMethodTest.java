51ddd92f945d4ff63af936370c53719f21ec8d0d
programer5 <neverdie4757@gmail.com>
2021-12-04 20:18:24 +0900

    템플릿 메서드 패턴 - 예제2

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,7 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +35,17 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
 
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethod1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic1();
+        template2.execute();
     }
 }
